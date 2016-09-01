53a5e02bd5f6ffc7da91f0416c6e005579393435
praganavictor <victormerigue@gmail.com>
2016-09-01 18:13:33 -0300

    Versao final

--- jogo.rb
+++ jogo.rb
@@ -4,11 +4,25 @@ def da_boas_vindas
 	nome = gets.strip
 	puts "\n\n"
 	puts "Começaremos o jogo para você, #{nome}"
+	nome
 end
 
-def sorteia_numero_secreto
-	puts "Escolhendo um número secreto entre 0 e 200..."
-	sorteado = 175
+def sorteia_numero_secreto(dificuldade)
+	case dificuldade
+	when 1
+		maximo = 30
+	when 2
+		maximo = 60
+	when 3
+		maximo = 100
+	when 4
+		maximo = 150
+	when 5
+		maximo = 200
+	end
+
+	puts "Escolhendo um número secreto entre 1 e #{maximo}..."
+	sorteado = rand(maximo) + 1
 	puts "Escolhido... que tal adivinhar hoje nosso número secreto?"	
 	sorteado
 end
@@ -40,27 +54,53 @@ def verifica_se_acertou(numero_secreto, chute)
     false
 end
 
+def pede_dificuldade
+	puts "Qual o nivel de dificuldade que deseja? (1 facil, 5 dificil)"
+	dificuldade = gets.to_i
+end
+def joga(nome, dificuldade)
+	numero_secreto = sorteia_numero_secreto dificuldade
+
+	pontos_ate_agora = 1000
+	limite_de_tentativas = 5
+
+	chutes = []
+
+	for tentativa in 1..limite_de_tentativas
+
+		chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+	    chutes << chute
+
+	    if nome == "Victor"
+	    	puts "Acertou"
+	    	break
+	    end
+	    
+	    pontos_a_perder = (chute - numero_secreto).abs / 2.0
+	    pontos_ate_agora -= pontos_a_perder
+
+		if verifica_se_acertou numero_secreto, chute
+			break
+		end
+	end
+
+	puts "Você ganhou #{pontos_ate_agora} pontos."
+end
+
+def nao_quer_jogar?
+	puts "Deseja jogar novamente? (S/N)"
+	quer_jogar = gets.strip
+	nao_quer_jogar = quer_jogar.upcase == "N"
+	
+end
 
 
-da_boas_vindas
-numero_secreto = sorteia_numero_secreto
+nome = da_boas_vindas
+dificuldade = pede_dificuldade
 
-pontos_ate_agora = 1000
-limite_de_tentativas = 5
-
-chutes = []
-
-for tentativa in 1..limite_de_tentativas
-
-	chute = pede_um_numero chutes, tentativa, limite_de_tentativas
-    chutes << chute
-    
-    pontos_a_perder = (chute - numero_secreto).abs / 2.0
-    pontos_ate_agora -= pontos_a_perder
-
-	if verifica_se_acertou numero_secreto, chute
+loop do
+	joga nome, dificuldade	
+	if nao_quer_jogar?
 		break
 	end
 end
-
-puts "Você ganhou #{pontos_ate_agora} pontos."
